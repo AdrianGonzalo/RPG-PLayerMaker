460216f9a1ccdb891084aeb069d18ac75c785231
Adrián <adriangonzalodev@gmail.com>
2025-01-29 15:52:14 +0100

    Completed the section race #2

--- rpg-player-maker/src/app/component/Race.jsx
+++ rpg-player-maker/src/app/component/Race.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { razas } from "../utils/Race";
 
 const Race = () => {
-  const [selectedRace, setSelectedRace] = useState("null");
+  const [selectedRace, setSelectedRace] = useState("null"); // estado color
+  const [selectedSubrace, setSelectedSubrace] = useState("null"); // estado subraza
 
   const handleSelectRace = (race) => {
     setSelectedRace(race);
+  };
+
+  const handleSelectSubrace = (subrace) => {
+    setSelectedSubrace(subrace);
   };
 
   return (
@@ -13,105 +19,47 @@ const Race = () => {
         Elige tu Raza
       </p>
       <div className="grid grid-cols-3 gap-2 mb-10">
-        <button
-          onClick={() => handleSelectRace("ELFO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "ELFO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          ELFO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("TIEFLING")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "TIEFLING"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          TIEFLING
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("HUMANO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "HUMANO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          HUMANO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("SEMIORCO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "SEMIORCO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          SEMIORCO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("ENANO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "ENANO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          ENANO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("SEMIELFO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "SEMIELFO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          SEMIELFO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("MEDIANO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "MEDIANO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          MEDIANO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("GNOMO")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "GNOMO"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          GNOMO
-        </button>
-
-        <button
-          onClick={() => handleSelectRace("DRAGONBORN")}
-          className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
-            selectedRace === "DRAGONBORN"
-              ? "bg-blue-500 text-[var(--color5)]" // está seleccionado
-              : "bg-[var(--color2)]" // no está seleccionado
-          }`}
-        >
-          DRAGONBORN
-        </button>
+        {Object.keys(razas).map((raceKey) => {
+          const race = razas[raceKey];
+          return (
+            <button
+              key={raceKey}
+              onClick={() => handleSelectRace(raceKey)}
+              className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
+                selectedRace === raceKey
+                  ? "bg-blue-500 text-[var(--color5)]"
+                  : "bg-[var(--color2)]"
+              }`}
+            >
+              {race.nombre}
+            </button>
+          );
+        })}
       </div>
+
+      {selectedRace !== "null" && razas[selectedRace].subrazas && (
+        <div className="mt-5 mb-6">
+          <p className="text-3xl mb-4 text-center">Elige tu Subraza</p>
+          <div className="grid grid-cols-3 gap-2">
+            {Object.keys(razas[selectedRace].subrazas).map((subraceKey) => {
+              const subrace = razas[selectedRace].subrazas[subraceKey];
+              return (
+                <button
+                  key={subraceKey}
+                  onClick={() => handleSelectSubrace(subraceKey)}
+                  className={`w-[200px] bg-[var(--color2)] text-xl p-1 rounded-lg ${
+                    selectedSubrace === subraceKey
+                      ? "bg-blue-500 text-[var(--color5)]"
+                      : "bg-[var(--color2)]"
+                  }`}
+                >
+                  {subrace.nombre}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
