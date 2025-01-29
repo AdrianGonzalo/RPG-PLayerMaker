1e2b9f6e7f31a18408ca91267b5b14034546cc5a
Adrián <adriangonzalodev@gmail.com>
2025-01-29 17:28:37 +0100

    change text in presentation #1

--- rpg-player-maker/src/app/component/Flashwindow.jsx
+++ rpg-player-maker/src/app/component/Flashwindow.jsx
@@ -16,22 +16,23 @@ const Window = () => {
       {visible && (
         <div className="flex justify-center items-center bg-[var(--background)]">
           <div className="bg-[var(--color2)] m-20 rounded-xl w-[1000px] h-auto">
-            <div className="flex flex-col items-center m-5">
+            <div className="flex flex-col items-center m-5 p-2">
               <p className="text-5xl flex justify-center mt-4">
                 Bienvenidos aventureros
               </p>
-
               <br></br>
-
-              <p className="text-2xl">
+              <p className="text-2xl text-center">
                 En esta aplicación podréis crear a vuestro aventurero favorito
-                eligiendo raza, subraza, clase, subclase y trasfondo. También la
-                aplicación os irá guiando con las ventajas o desventajas que
-                tiene cada raza, clase... El aventurero por ahora será lv 1, en
-                un futuro se le podrán subir niveles. Disfrutad y gracias por
-                confiar en mi &lt;3 :)
+                eligiendo nombre, sexo, raza, subraza, clase, subclase,
+                trasfondo, atributos y vida. También la aplicación os irá
+                guiando con las ventajas que tiene cada raza, clase... El
+                aventurero será lv 1 y siguiendo las normas de D&D 5e.
               </p>
-
+              <br></br>
+              <p className="text-2xl text-center mt-4">
+                Gracias por confiar en mi{" "}
+                <span className="text-red-500">&lt;3</span> :)
+              </p>
               <Button onClick={handleButtonClick}>Forjar a tu Heroe</Button>
             </div>
           </div>
