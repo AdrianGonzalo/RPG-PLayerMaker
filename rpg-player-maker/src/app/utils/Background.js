205c75b9a9525543deaf4407a3936ce97384e821
Adrián <adriangonzalodev@gmail.com>
2025-02-03 18:02:33 +0100

    add new js the background with information and structure the class component like the others #4

---
+++ rpg-player-maker/src/app/utils/Background.js
@@ -0,0 +1,37 @@
+export const background = {
+  acólito: {
+    name: "Acólito",
+    description:
+      "Has pasado tu vida en un templo, aprendiendo sobre una deidad y sirviéndola.",
+    features: ["Rituales religiosos", "Idioma adicional"],
+  },
+  criminal: {
+    name: "Criminal",
+    description:
+      "Has vivido una vida de crimen, desde robos hasta asociaciones con bandas peligrosas.",
+    features: ["Contactos", "Sigilo"],
+  },
+  soldado: {
+    name: "Soldado",
+    description:
+      "Has servido en un ejército, aprendiendo tácticas y habilidades marciales.",
+    features: ["Tácticas de batalla", "Soldados veteranos"],
+  },
+  erudito: {
+    name: "Erudito",
+    description:
+      "Has dedicado tu vida a la búsqueda del conocimiento y la investigación académica.",
+    features: ["Conocimiento enciclopédico", "Idioma adicional"],
+  },
+  marinero: {
+    name: "Marinero",
+    description: "Pasaste tu vida navegando por los mares y surcando las olas.",
+    features: ["Navegación", "Tareas náuticas"],
+  },
+  heroeDelPueblo: {
+    name: "Héroe del Pueblo",
+    description:
+      "Eres un luchador que ha defendido a tu gente, tal vez de algún desastre o invasión.",
+    features: ["Inspiración popular", "Resistencia al miedo"],
+  },
+};
--- rpg-player-maker/src/app/utils/Classs.js
+++ rpg-player-maker/src/app/utils/Classs.js
@@ -1,29 +1,27 @@
-export const clases = [
-  {
-    //Bárbaro
+export const clases = {
+  barbaro: {
     name: "Bárbaro",
     hitDie: "d12",
     primaryAbility: "Fuerza",
     weapons: "Armas simples y marciales",
     savingThrows: ["Fuerza", "Constitución"],
     features: ["Rabia", "Defensa sin armadura"],
-    subclasses: [
-      {
+    subclasses: {
+      berserker: {
         name: "Camino del Berserker",
         features: ["Frenesí", "Mente imperturbable"],
       },
-      {
+      totemico: {
         name: "Camino del Guerrero Totémico",
         features: ["Espíritu Totémico", "Aspecto de la bestia"],
       },
-      {
+      ancestral: {
         name: "Camino del Ancestral",
         features: ["Guardianes espirituales", "Guía ancestral"],
       },
-    ],
+    },
   },
-  {
-    //Bardo
+  bardo: {
     name: "Bardo",
     hitDie: "d8",
     primaryAbility: "Carisma",
@@ -31,228 +29,219 @@ export const clases = [
       "Armas simples, ballesta de mano, espada larga, estoque, espada corta",
     savingThrows: ["Destreza", "Carisma"],
     features: ["Inspiración bárdica", "Lanzamiento de conjuros"],
-    subclasses: [
-      {
+    subclasses: {
+      espada: {
         name: "Colegio de la Espada",
         features: ["Estilo de combate", "Floritura de espada"],
       },
-      {
+      saber: {
         name: "Colegio del Saber",
         features: ["Conocimiento adicional", "Cortar palabras"],
       },
-      {
+      glamour: {
         name: "Colegio del Glamour",
         features: ["Presencia majestuosa", "Manto de inspiración"],
       },
-      {
+      espiritu: {
         name: "Colegio de los Espíritus",
         features: ["Rituales espirituales", "Hablar con los muertos"],
       },
-    ],
+    },
   },
-  {
-    //Clérigo
+  clerigo: {
     name: "Clérigo",
     hitDie: "d8",
     primaryAbility: "Sabiduría",
     weapons: "Armas simples",
     savingThrows: ["Sabiduría", "Carisma"],
     features: ["Magia divina", "Canalizar divinidad"],
-    subclasses: [
-      {
+    subclasses: {
+      vida: {
         name: "Dominio de la Vida",
         features: ["Discípulo de la vida", "Sanación divina"],
       },
-      {
+      tempestad: {
         name: "Dominio de la Tempestad",
         features: ["Furia de la tormenta", "Ira del trueno"],
       },
-      {
+      muerte: {
         name: "Dominio de la Muerte",
         features: ["Toque de la tumba", "Aura de la muerte"],
       },
-      {
+      guerra: {
         name: "Dominio de la Guerra",
         features: ["Golpe guiado", "Arsenal divino"],
       },
-      {
+      engano: {
         name: "Dominio del Engaño",
         features: ["Ilusión divina", "Sombras bendecidas"],
       },
-    ],
+    },
   },
-  {
-    //Druida
+  druida: {
     name: "Druida",
     hitDie: "d8",
     primaryAbility: "Sabiduría",
     weapons: "Armas simples, guadañas, hondas, bastones y lanzas",
     savingThrows: ["Inteligencia", "Sabiduría"],
     features: ["Forma salvaje", "Lanzamiento de conjuros"],
-    subclasses: [
-      {
+    subclasses: {
+      tierra: {
         name: "Círculo de la Tierra",
         features: ["Magia del círculo", "Recuperación natural"],
       },
-      {
+      luna: {
         name: "Círculo de la Luna",
         features: ["Forma salvaje mejorada", "Forma lunar"],
       },
-      {
+      suenos: {
         name: "Círculo de los Sueños",
         features: ["Bendiciones feéricas", "Paso onírico"],
       },
-    ],
+    },
   },
-  {
-    //Explorador
+  explorador: {
     name: "Explorador",
     hitDie: "d10",
     primaryAbility: "Destreza y Sabiduría",
     weapons: "Todas las armas simples y marciales",
     savingThrows: ["Fuerza", "Destreza"],
     features: ["Enemigo predilecto", "Explorador natural"],
-    subclasses: [
-      {
+    subclasses: {
+      cazador: {
         name: "Cazador",
         features: ["Golpe poderoso", "Reflejos sobrenaturales"],
       },
-      {
+      bestias: {
         name: "Maestro de bestias",
         features: ["Vínculo con la bestia", "Comando animal"],
       },
-    ],
+    },
   },
-  {
-    //Guerrero
+  guerrero: {
     name: "Guerrero",
     hitDie: "d10",
     primaryAbility: "Fuerza o Destreza",
     weapons: "Todas las armas simples y marciales",
     savingThrows: ["Fuerza", "Constitución"],
     features: ["Ataque adicional", "Estilo de combate"],
-    subclasses: [
-      {
+    subclasses: {
+      batalla: {
         name: "Maestro de batalla",
         features: ["Maniobras", "Dados de superioridad"],
       },
-      {
+      campeon: {
         name: "Campeón",
         features: ["Golpe crítico mejorado", "Atleta excepcional"],
       },
-    ],
+    },
   },
-  {
-    //Monje
+  monje: {
     name: "Monje",
     hitDie: "d8",
     primaryAbility: "Destreza y Sabiduría",
     weapons: "Armas simples y espadas cortas",
     savingThrows: ["Fuerza", "Destreza"],
     features: ["Artes marciales", "Defensa sin armadura"],
-    subclasses: [
-      {
+    subclasses: {
+      mano: {
         name: "Camino de la Mano Abierta",
         features: ["Palma de fuerza", "Maestría en ki"],
       },
-      {
+      sombra: {
         name: "Camino de la Sombra",
         features: ["Sigilo absoluto", "Teletransporte de sombra"],
       },
-    ],
+    },
   },
-  {
-    //Paladín
+  paladin: {
     name: "Paladín",
     hitDie: "d10",
     primaryAbility: "Fuerza y Carisma",
     weapons: "Todas las armas simples y marciales",
     savingThrows: ["Sabiduría", "Carisma"],
     features: ["Imposición de manos", "Castigo divino"],
-    subclasses: [
-      {
+    subclasses: {
+      devocion: {
         name: "Juramento de Devoción",
         features: ["Aura de protección", "Luz sagrada"],
       },
-      {
+      venganza: {
         name: "Juramento de Venganza",
         features: ["Sentencia del vengador", "Ataque implacable"],
       },
-    ],
+    },
   },
-  {
-    //Brujo
+  brujo: {
     name: "Brujo",
     hitDie: "d8",
     primaryAbility: "Carisma",
     weapons: "Armas simples",
     savingThrows: ["Sabiduría", "Carisma"],
     features: ["Pacto mágico", "Invocaciones élficas"],
-    subclasses: [
-      {
+    subclasses: {
+      archifey: {
         name: "Patrón Archifey",
         features: ["Encantamiento faérico", "Sombra ilusoria"],
       },
-      {
+      infernal: {
         name: "Patrón Infernal",
         features: ["Resistencia infernal", "Poder del diablo"],
       },
-    ],
+    },
   },
-  {
+  mago: {
     name: "Mago",
     hitDie: "d6",
     primaryAbility: "Inteligencia",
     weapons: "Dagas, dardos, hondas, bastones y ballestas ligeras",
     savingThrows: ["Inteligencia", "Sabiduría"],
     features: ["Lanzamiento de conjuros", "Recuperación arcana"],
-    subclasses: [
-      {
+    subclasses: {
+      evocacion: {
         name: "Escuela de Evocación",
         features: ["Moldear conjuros", "Explosión potenciada"],
       },
-    ],
+    },
   },
-  {
-    // Hechicero
+  hechicero: {
     name: "Hechicero",
     hitDie: "d6",
     primaryAbility: "Carisma",
     weapons: "Dagas, dardos, hondas, bastones y ballestas ligeras",
     savingThrows: ["Constitución", "Carisma"],
     features: ["Magia salvaje", "Recuperación arcana"],
-    subclasses: [
-      {
+    subclasses: {
+      draconico: {
         name: "Linaje dracónico",
         features: ["Resistencia dracónica", "Aliento dracónico"],
       },
-      {
+      magia: {
         name: "Hechicería de la magia salvaje",
         features: ["Surge de magia salvaje", "Magia incontrolable"],
       },
-    ],
+    },
   },
-  {
-    // Pícaro
+  picaro: {
     name: "Pícaro",
     hitDie: "d8",
     primaryAbility: "Destreza",
     weapons: "Armas simples, ballesta de mano, espadas cortas, dagas, y arcos",
     savingThrows: ["Destreza", "Inteligencia"],
     features: ["Trucos", "Evasión"],
-    subclasses: [
-      {
+    subclasses: {
+      ladron: {
         name: "Ladrón",
         features: ["Robo sigiloso", "Ataque furtivo"],
       },
-      {
+      asesino: {
         name: "Asesino",
         features: ["Golpe mortal", "Imitación del engaño"],
       },
-      {
+      arcano: {
         name: "Explorador Arcano",
         features: ["Tácticas letales", "Rapidez"],
       },
-    ],
+    },
   },
-];
+};
