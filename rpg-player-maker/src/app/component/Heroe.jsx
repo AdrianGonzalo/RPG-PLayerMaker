05a74189e12ed329e35f65661584b5d953a55ad5
Adrián <adriangonzalodev@gmail.com>
2025-02-03 18:33:11 +0100

    create the heroe component #2

---
+++ rpg-player-maker/src/app/component/Heroe.jsx
@@ -0,0 +1,41 @@
+const Heroe = ({ character }) => {
+  return (
+    <div className="flex justify-center items-center flex-col text-2xl">
+      <div className="bg-[var(--color5)] rounded-xl w-[1000px] h-auto border-[var(--bordercreate)] border-2 m-20 text-center p-6">
+        <p className="text-[70px] mb-10 mt-6 text-[var(--color3)]">
+          {character.name}
+        </p>
+        <p className="mb-10">
+          {character.race}[{character.subrace}]
+        </p>
+        <div className="flex justify-center">
+          <div className="flex flex-col justify-center">
+            <p>Fuerza 13</p>
+            <p>Destreza 16</p>
+            <p>Constitucion 13</p>
+            <p>Inteligencia 11</p>
+            <p>Sabiduria 6</p>
+            <p>Carisma 8</p>
+          </div>
+          <div className="bg-black w-[200px] h-[200px] p-16 m-6 ">Imagen</div>
+          <div className="flex flex-col justify-center">
+            <p>9 Vida</p>
+            <p>30 Velocidad</p>
+            <p>60 Vision oscuridad</p>
+          </div>
+        </div>
+        <p className="mb-4">Sexo: {character.sex}</p>
+        <p className="mb-4">
+          Clase: {character.clase}{" "}
+          {character.subclass ? `[${character.subclass}]` : ""}
+        </p>
+        <p className="mb-4">
+          Trasfondo:{" "}
+          {character.background ? character.background.name : "No seleccionado"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Heroe;
