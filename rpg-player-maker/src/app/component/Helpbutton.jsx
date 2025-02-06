5c22303e5dc2bf797cbcf82a24a9e818b3ff6c81
Adrián <adriangonzalodev@gmail.com>
2025-02-06 12:34:12 +0100

    change text and fix funcionality #3

--- rpg-player-maker/src/app/component/Helpbutton.jsx
+++ rpg-player-maker/src/app/component/Helpbutton.jsx
@@ -12,123 +12,105 @@ const Helpbutton = () => {
   return (
     <>
       {visible && (
-        <div className="flex justify-center items-center bg-[var(--background)]">
-          <div className="bg-[var(--color2)] m-20 rounded-xl w-[1000px] h-[1300px]">
-            <div className="flex flex-col items-center p-10 gap-10 ">
-              <p className="text-3xl">
+        <div className="fixed inset-0 flex justify-center items-center bg-[var(--background)] z-40">
+          <div className="bg-[var(--color2)] m-20 rounded-xl w-[1000px] h-auto border-double border-[10px] border-[var(--color6)]">
+            <div className="flex flex-col items-center p-10 gap-10 text-[17px]">
+              <p className="text-3xl underline decoration-2 underline-offset-4 text-[var(--color4)]">
                 ¿Tienes dudas sobre como crear tu personaje?
               </p>
-              <p>
+              <p className="text-xl">
                 No te preocupes, nuestro escudero de confianza Timmy te
                 explicara todo lo que necesites saber para crear tu personaje
                 paso a paso.
               </p>
               <div className="flex justify-around gap-10">
                 <div>
-                  {/* <div>
-                    <p className="text-2xl mb-2">• Nombre de tu heroe</p>
-                    <p>
-                      En esta parte podras poner el nombre que mas represente a
-                      tu heroe.
-                    </p>
-                  </div>
-                  <br></br>
                   <div>
-                    <p className="text-2xl mb-2">• Elige tu Sexo</p>
-                    <p>
-                      En esta seccion podras elegir el sexo de tu personaje.
+                    <p className="text-2xl mb-2 text-[var(--color4)]">
+                      • Que es una raza?
                     </p>
-                  </div>
-                  <br></br> */}
-                  <div>
-                    <p className="text-2xl mb-2">• Elige tu Raza</p>
+                    <p>
+                      Una raza define la herencia biológica y cultural de un
+                      personaje, otorgándole rasgos especiales, atributos,
+                      idioma y habilidades únicas que influyen en su historia y
+                      jugabilidad.
+                    </p>
                     <p>
                       En este apartado podras elegir entre una multitud de
                       razas, de las cuales, elfo, enano y gnomo tienen otra
                       eleccion mas para elegir, la subraza.
                     </p>
                     <p>
-                      En cuanto selecciones la raza y subraza si la seleccionada
-                      tiene, podreis ver los bonificadores con los que cuenta,
-                      los pasos, vision en la oscuridad...
+                      Cada raza y subraza tiene unas habilidades propias unicas,
+                      y a medida que eliges puedes verlas para comparar y elegir
+                      la que mas se ajusta a tu heroe.
                     </p>
-                    <p>Elige la que mas se adecue a tu heroe. </p>
                   </div>
                   <br></br>
                   <div>
-                    <p className="text-2xl mb-2">• Elige tu Clase</p>
+                    <p className="text-2xl mb-2 text-[var(--color4)]">
+                      • Que es una clase?
+                    </p>
+                    <p>
+                      Una clase representa la profesión y estilo de combate de
+                      un personaje, determinando sus habilidades , equipo, magia
+                      y progresion a lo largo del juego.
+                    </p>
                     <p>
                       En esta eleccion podras elegir entre una multitud de
                       clases, de las cuales, todas tendran su respectiva
                       subclase.
                     </p>
                     <p>
-                      En cuanto selecciones la clase y subclase, podreis ver
-                      toda la informacion necesaria, dado de daño, habilidad
-                      primaria, competencia con armas....
-                    </p>
-                    <p>
-                      Cada clase y subclase otorgan características distintas,
-                      por lo que elige la que mas se adecue a tu heroe.
+                      Cada clase y subclase tiene unas habilidades propias
+                      unicas, y a medida que eliges puedes verlas para comparar
+                      y elegir la que mas se ajusta a tu heroe.
                     </p>
                   </div>
                   <br></br>
                   <div>
-                    <p className="text-2xl mb-2">• Elige tu Trasfondo</p>
-                    <p>
-                      En una partida de rol es importante tener un pasado y
-                      justamente el trasfondo te ayuda a ello.
+                    <p className="text-2xl mb-2 text-[var(--color4)]">
+                      • Ques es el trasfondo?
                     </p>
                     <p>
-                      Tendras una pequeña y general definicion del trasfondo
-                      escogido para que complementes la historia de tu
-                      personaje.
+                      El Trasfondo es la historia y origen de un personaje antes
+                      de comenzar tu aventura.
                     </p>
                     <p>
-                      Dependiendo el trasfondo elegido obtedras unas habilidades
-                      o otras.
-                    </p>
-                    <p>
-                      Piensa en el pasado de tu personaje y elige la que mas se
-                      adecue a el.
+                      Define sus habilidades, motivaciones... ayudando a darle
+                      profundidad y contexto dentro del juego.
                     </p>
                   </div>
                   <br></br>
                   <div>
-                    <p className="text-2xl mb-2">• Crea tus atributos</p>
+                    <p className="text-2xl mb-2 text-[var(--color4)]">
+                      • Como crear tus atributos?
+                    </p>
                     <p>
-                      La parte mas importante de tu personaje, los atributos,
-                      para saber que atributos tendra tu personaje se tiran 4
-                      dados d6 (6 caras) y de esas 4 tiradas se descarta la mas
-                      baja y se suman las demas tiradas dejando un total de x,
-                      asi para cada atributo.
+                      Los atributos son la parte mas importante de tu personaje,
+                      definirlos es muy sencillo, hay que tirar un d6 cuatro
+                      veces, la tirada mas baja se descarta, y la suma de las
+                      otras tres tiradas hacen el atributo, asi hasta
+                      completarlos todos.
                     </p>
                     <p>
                       Debido a que es puro azar y esto en las normas no esta,
                       hemos decidido añadir un boton para resear las tiradas
                       siempre y cuando no se haya completado la cuarta.
                     </p>
-                    <p>Por lo demas, buena suerte :)</p>
                   </div>
                   <br></br>
                   <div>
-                    <p className="text-2xl mb-2">
-                      • Cuanta Vida tendra tu personaje
+                    <p className="text-2xl mb-2 text-[var(--color4)]">
+                      • Cuanta Vida tendra tu personaje ?
                     </p>
                     <p>
-                      Despues de elegir todo y saber tus atributos te queda
-                      saber cuanta vida tendra tu personaje y para suerte de ti,
-                      jugador, no sera al azar, sino que sera una simple suma
-                      entre el maximo numero de tu dado de golpe y tu
-                      constitución.
+                      Para saber la vida que tendra tu personaje, para suerte de
+                      ti, jugador, no sera al azar, sino que se trata de una
+                      suma entre tu maximo del dado de daño y tu constitución.
                     </p>
                   </div>
                   <br></br>
-                  <p>
-                    Despues de toda esta chapa ya estaras listo para afrontar tu
-                    camino a las tierras intermedias, mucha suerte y que nuestro
-                    señor te acompañe.
-                  </p>
                 </div>
                 <div className="w-[500px] h-[300px] bg-black"></div>
               </div>
@@ -137,7 +119,7 @@ const Helpbutton = () => {
         </div>
       )}
 
-      <div className="fixed bottom-4 right-4">
+      <div className="fixed bottom-4 right-4 z-50">
         <button
           onClick={handlehelpbutton}
           className="h-[80px] w-[80px]  rounded-full border-yellow-300 border-double border-4 flex items-center justify-center"
