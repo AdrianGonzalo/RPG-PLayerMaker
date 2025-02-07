fa6119731725b7486192670383e8c8ec364730ea
Adrián <adriangonzalodev@gmail.com>
2025-02-07 17:35:33 +0100

    Add a image in the helpButton section #3

--- rpg-player-maker/src/app/component/Helpbutton.jsx
+++ rpg-player-maker/src/app/component/Helpbutton.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 
 const Helpbutton = () => {
   const [visible, setVisible] = useState(false); // Inicializa el estado como false
@@ -13,64 +14,60 @@ const Helpbutton = () => {
     <>
       {visible && (
         <div className="fixed inset-0 flex justify-center items-center bg-[var(--background)] z-40">
-          <div className="bg-[var(--color2)] m-20 rounded-xl w-[1000px] h-auto border-double border-[10px] border-[var(--color6)]">
-            <div className="flex flex-col items-center p-10 gap-10 text-[17px]">
+          <div className="bg-[var(--color2)] m-10 rounded-xl w-[1150px] h-auto border-double border-[10px] border-[var(--color6)]">
+            <div className="flex flex-col items-center p-10 gap-4 text-[17px]">
               <p className="text-3xl underline decoration-2 underline-offset-4 text-[var(--color4)]">
                 ¿Tienes dudas sobre como crear tu personaje?
               </p>
               <p className="text-xl">
-                No te preocupes, nuestro escudero de confianza Timmy te
-                explicara todo lo que necesites saber para crear tu personaje
-                paso a paso.
+                No te preocupes, nuestros escuderos de confianza Borgroth y
+                Madness te explicaran todo lo que necesites saber.
               </p>
-              <div className="flex justify-around gap-10">
+              <div>
+                <p className="text-2xl mb-2 text-[var(--color4)]">
+                  • Que es una raza?
+                </p>
+                <p>
+                  Una raza define la herencia biológica y cultural de un
+                  personaje, otorgándole rasgos especiales, atributos, idioma y
+                  habilidades únicas que influyen en su historia y jugabilidad.
+                </p>
+                <p>
+                  En este apartado podras elegir entre una multitud de razas, de
+                  las cuales, elfo, enano y gnomo tienen otra eleccion mas para
+                  elegir, la subraza.
+                </p>
+                <p>
+                  Cada raza y subraza tienen habilidades unicas, y a medida que
+                  eliges puedes verlas para comparar y elegir la que mas se
+                  ajusta a tu heroe.
+                </p>
+              </div>
+              <div className="flex justify-around">
                 <div>
-                  <div>
-                    <p className="text-2xl mb-2 text-[var(--color4)]">
-                      • Que es una raza?
-                    </p>
-                    <p>
-                      Una raza define la herencia biológica y cultural de un
-                      personaje, otorgándole rasgos especiales, atributos,
-                      idioma y habilidades únicas que influyen en su historia y
-                      jugabilidad.
-                    </p>
-                    <p>
-                      En este apartado podras elegir entre una multitud de
-                      razas, de las cuales, elfo, enano y gnomo tienen otra
-                      eleccion mas para elegir, la subraza.
-                    </p>
-                    <p>
-                      Cada raza y subraza tiene unas habilidades propias unicas,
-                      y a medida que eliges puedes verlas para comparar y elegir
-                      la que mas se ajusta a tu heroe.
-                    </p>
-                  </div>
-                  <br></br>
                   <div>
                     <p className="text-2xl mb-2 text-[var(--color4)]">
                       • Que es una clase?
                     </p>
                     <p>
                       Una clase representa la profesión y estilo de combate de
-                      un personaje, determinando sus habilidades , equipo, magia
+                      un personaje, determinando sus habilidades, equipo, magia
                       y progresion a lo largo del juego.
                     </p>
                     <p>
                       En esta eleccion podras elegir entre una multitud de
-                      clases, de las cuales, todas tendran su respectiva
-                      subclase.
+                      clases, con sus respectivas subclases.
                     </p>
                     <p>
-                      Cada clase y subclase tiene unas habilidades propias
-                      unicas, y a medida que eliges puedes verlas para comparar
-                      y elegir la que mas se ajusta a tu heroe.
+                      Cada clase y subclase tienen habilidades unicas, y a
+                      medida que eliges puedes verlas para comparar y elegir la
+                      que mas se ajusta a tu heroe.
                     </p>
                   </div>
                   <br></br>
                   <div>
                     <p className="text-2xl mb-2 text-[var(--color4)]">
-                      • Ques es el trasfondo?
+                      • Que es el trasfondo?
                     </p>
                     <p>
                       El Trasfondo es la historia y origen de un personaje antes
@@ -93,11 +90,6 @@ const Helpbutton = () => {
                       otras tres tiradas hacen el atributo, asi hasta
                       completarlos todos.
                     </p>
-                    <p>
-                      Debido a que es puro azar y esto en las normas no esta,
-                      hemos decidido añadir un boton para resear las tiradas
-                      siempre y cuando no se haya completado la cuarta.
-                    </p>
                   </div>
                   <br></br>
                   <div>
@@ -112,7 +104,13 @@ const Helpbutton = () => {
                   </div>
                   <br></br>
                 </div>
-                <div className="w-[500px] h-[300px] bg-black"></div>
+                <Image
+                  src="/ImageSoldiers.png"
+                  alt="image the soldiers"
+                  width={400}
+                  height={200}
+                  className="p-6"
+                />
               </div>
             </div>
           </div>
