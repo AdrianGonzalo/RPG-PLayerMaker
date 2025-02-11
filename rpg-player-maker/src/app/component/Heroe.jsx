fa444c2877d9bee9d3be5f2be7d0f28f0ad6854b
Adrián <adriangonzalodev@gmail.com>
2025-02-11 18:27:35 +0100

    complete the life and implement in the heroe.jsx  #2

--- rpg-player-maker/src/app/component/Heroe.jsx
+++ rpg-player-maker/src/app/component/Heroe.jsx
@@ -1,4 +1,14 @@
+import { clases } from "../utils/Classs";
+
 const Heroe = ({ character }) => {
+  const selectedClass = clases[character.clase];
+  const hitDieMax = selectedClass
+    ? parseInt(selectedClass.hitDie.replace("d", ""))
+    : 0;
+  const constitutionScore = character.attributes?.["Constitución"] || 10;
+  const constitutionModifier = Math.floor((constitutionScore - 10) / 2);
+  const totalHP = hitDieMax + constitutionModifier;
+
   return (
     <div className="flex justify-center items-center flex-col text-2xl">
       <div className="bg-[var(--color5)] rounded-xl w-[1000px] h-auto border-[var(--bordercreate)] border-2 m-20 text-center p-6">
@@ -12,16 +22,16 @@ const Heroe = ({ character }) => {
           <div className="flex flex-col justify-center">
             <p>Fuerza {character.attributes.Fuerza}</p>
             <p>Destreza {character.attributes.Destreza}</p>
-            <p>Constitucion {character.attributes.Constitución}</p>
+            <p>Constitución {character.attributes.Constitución}</p>
             <p>Inteligencia {character.attributes.Inteligencia}</p>
-            <p>Sabiduria {character.attributes.Sabiduría}</p>
+            <p>Sabiduría {character.attributes.Sabiduría}</p>
             <p>Carisma {character.attributes.Carisma}</p>
           </div>
           <div className="bg-black w-[200px] h-[200px] p-16 m-6 ">Imagen</div>
           <div className="flex flex-col justify-center">
-            <p>9 Vida</p>
+            <p>{totalHP} Vida</p>
             <p>{character.speed} Velocidad</p>
-            <p>{character.darkVision} Vision oscuridad</p>
+            <p>{character.darkVision} Visión en la oscuridad</p>
           </div>
         </div>
         <p className="mb-4">Sexo: {character.sex}</p>
--- rpg-player-maker/src/app/component/Life.jsx
+++ rpg-player-maker/src/app/component/Life.jsx
@@ -1,14 +1,31 @@
-const Life = () => {
+import { clases } from "../utils/Classs";
+
+const Life = ({ character }) => {
+  const selectedClass = clases[character.clase];
+
+  const hitDieMax = selectedClass
+    ? parseInt(selectedClass.hitDie.replace("d", ""))
+    : 0;
+
+  const constitutionScore = character.attributes?.["Constitución"] || 10;
+  const constitutionModifier = Math.floor((constitutionScore - 10) / 2);
+
+  // Calcular los puntos de vida
+  const totalHP = hitDieMax + constitutionModifier;
+
   return (
     <div>
       <p className="text-4xl mb-8 w-full text-center underline decoration-2 underline-offset-4">
-        Cuanta Vida tendra tu personaje?
+        ¿Cuánta vida tendrá tu personaje?
       </p>
       <div className="text-center bg-[var(--color2)] px-8 py-2 rounded-lg">
         <p className="text-2xl">
-          Máximo del dado de golpe + modificador de Constitución
+          Máximo del dado de golpe ({hitDieMax}) + Modificador de Constitución (
+          {constitutionModifier})
         </p>
-        <p className="text-2xl"> X + Y = Z hp</p>
+        <p className="text-2xl">
+          {hitDieMax} + {constitutionModifier} = {totalHP} HP
+        </p>
       </div>
     </div>
   );
