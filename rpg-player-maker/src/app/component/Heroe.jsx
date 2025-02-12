2503a8a1b651af20f2238a81ece614e86a5324d3
Adrián <adriangonzalodev@gmail.com>
2025-02-12 07:56:23 +0100

    fixed the layout of total attributes by adding the bonuses of each race #2

--- rpg-player-maker/src/app/component/Heroe.jsx
+++ rpg-player-maker/src/app/component/Heroe.jsx
@@ -1,4 +1,5 @@
 import { clases } from "../utils/Classs";
+import { razas } from "../utils/Race";
 
 const Heroe = ({ character }) => {
   const selectedClass = clases[character.clase];
@@ -9,6 +10,25 @@ const Heroe = ({ character }) => {
   const constitutionModifier = Math.floor((constitutionScore - 10) / 2);
   const totalHP = hitDieMax + constitutionModifier;
 
+  // Obtener bonificadores de raza y subraza
+  const selectedRace = razas[character.race] || {};
+  const selectedSubrace = selectedRace.subrazas?.[character.subrace] || {};
+
+  const raceBonuses = selectedRace.bonificadores || {};
+  const subraceBonuses = selectedSubrace.bonificadores || {};
+
+  // Mapeo para nombres correctos
+  const attributeMap = {
+    Fuerza: "fuerza",
+    Destreza: "destreza",
+    Constitución: "constitucion",
+    Inteligencia: "inteligencia",
+    Sabiduría: "sabiduria",
+    Carisma: "carisma",
+  };
+
+  const attributeKeys = Object.keys(attributeMap);
+
   return (
     <div className="flex justify-center items-center flex-col text-2xl">
       <div className="bg-[var(--color5)] rounded-xl w-[1000px] h-auto border-[var(--bordercreate)] border-2 m-20 text-center p-6">
@@ -16,18 +36,30 @@ const Heroe = ({ character }) => {
           {character.name}
         </p>
         <p className="mb-10">
-          {character.race}[{character.subrace}]
+          {character.race}
+          {character.subrace ? ` [${character.subrace}]` : ""}
         </p>
         <div className="flex justify-center">
           <div className="flex flex-col justify-center">
-            <p>Fuerza {character.attributes.Fuerza}</p>
-            <p>Destreza {character.attributes.Destreza}</p>
-            <p>Constitución {character.attributes.Constitución}</p>
-            <p>Inteligencia {character.attributes.Inteligencia}</p>
-            <p>Sabiduría {character.attributes.Sabiduría}</p>
-            <p>Carisma {character.attributes.Carisma}</p>
+            {attributeKeys.map((attr) => {
+              const baseValue = character.attributes?.[attr] || 10;
+              const mappedKey = attributeMap[attr]; // Convertimos el nombre al formato correcto
+              const raceBonus = raceBonuses[mappedKey] || 0;
+              const subraceBonus = subraceBonuses[mappedKey] || 0;
+              const totalBonus = raceBonus + subraceBonus;
+              const totalValue = baseValue + totalBonus;
+
+              return (
+                <p key={attr}>
+                  {attr} {baseValue}
+                  {totalBonus > 0 && (
+                    <span className="text-green-500"> +{totalBonus}</span>
+                  )}
+                </p>
+              );
+            })}
           </div>
-          <div className="bg-black w-[200px] h-[200px] p-16 m-6 ">Imagen</div>
+          <div className="bg-black w-[200px] h-[200px] p-16 m-6">Imagen</div>
           <div className="flex flex-col justify-center">
             <p>{totalHP} Vida</p>
             <p>{character.speed} Velocidad</p>
