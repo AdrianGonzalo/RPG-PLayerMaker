e9c420ce7a84a5bd096aac2658c86962851df51c
Adrián <adriangonzalodev@gmail.com>
2025-02-12 19:12:31 +0100

    change css the heroe.jsx component WIP #3

--- rpg-player-maker/src/app/component/Heroe.jsx
+++ rpg-player-maker/src/app/component/Heroe.jsx
@@ -76,82 +76,90 @@ const Heroe = ({ character }) => {
 
         {character.background && (
           <div className="mt-4 bg-[var(--color6)] rounded-xl p-4 text-lg">
-            <p className="mb-4 font-bold">
+            <p className="mb-4 text-4xl text-[var(--color4)]">
               Trasfondo: {character.background.name || "No seleccionado"}
             </p>
-            <p className="font-bold text-xl mb-2">Descripción:</p>
-            <p>{character.background.description}</p>
-            <p className="font-bold text-xl mt-4 mb-2">Habilidades:</p>
+            <p className="text-2xl">{character.background.description}</p>
+            <p className="text-2xl mt-4 mb-2">• Habilidades:</p>
             <ul className="list-disc list-inside">
               {character.background.features.map((feature, index) => (
-                <li key={index}>{feature}</li>
+                <ul key={index}>{feature}</ul>
               ))}
             </ul>
           </div>
         )}
 
-        <div className="flex justify-center gap-6">
-          <div className="mt-6 p-4 bg-[var(--color4)] rounded-xl">
-            <p className="text-3xl mb-4">Bonificadores de Raza y Subraza</p>
-            {[raceBonuses, subraceBonuses].map(
-              (bonuses, idx) =>
-                bonuses &&
-                Object.keys(bonuses).length > 0 && (
-                  <div key={idx} className="mb-4">
-                    <p className="text-xl font-bold">
-                      {idx === 0
-                        ? "Bonificadores de Raza:"
-                        : "Bonificadores de Subraza:"}
-                    </p>
-                    <ul>
-                      {Object.entries(bonuses).map(([key, value]) => (
-                        <li key={key}>
-                          {key.charAt(0).toUpperCase() + key.slice(1)}: +{value}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )
-            )}
-          </div>
-
-          {selectedClass.name && (
-            <div className="mt-4 bg-[var(--color6)] rounded-xl p-4 text-lg">
-              <p className="text-xl font-bold">Características de Clase:</p>
-              <ul>
-                <li>
-                  <strong>Dado de golpe:</strong> {selectedClass.hitDie}
-                </li>
-                <li>
-                  <strong>Habilidad primaria:</strong>{" "}
-                  {selectedClass.primaryAbility}
-                </li>
-                <li>
-                  <strong>Armas:</strong> {selectedClass.weapons}
-                </li>
-                <li>
-                  <strong>Tiradas de salvación:</strong>{" "}
-                  {selectedClass.savingThrows?.join(", ")}
-                </li>
-                <li>
-                  <strong>Rasgos:</strong> {selectedClass.features?.join(", ")}
-                </li>
-              </ul>
-              {selectedSubclass.name && (
-                <div className="mt-4 bg-[var(--color6)] rounded-xl p-4 text-lg">
-                  <p className="text-xl font-bold">
-                    Características de Subclase: {selectedSubclass.name}
+        <div className="mt-6 p-4 bg-[var(--color6)] rounded-xl">
+          <p className="text-4xl mb-4 text-[var(--color4)]">
+            Bonificadores de Raza y Subraza
+          </p>
+          {[raceBonuses, subraceBonuses].map(
+            (bonuses, idx) =>
+              bonuses &&
+              Object.keys(bonuses).length > 0 && (
+                <div key={idx} className="mb-4">
+                  <p className="text-2xl">
+                    {idx === 0
+                      ? "• Bonificadores de Raza:"
+                      : "• Bonificadores de Subraza:"}
                   </p>
-                  <ul>
-                    <li>
-                      <strong>Rasgos:</strong>{" "}
-                      {selectedSubclass.features?.join(", ")}
-                    </li>
+                  <ul className="text-xl">
+                    {Object.entries(bonuses).map(([key, value]) => (
+                      <li key={key}>
+                        {key.charAt(0).toUpperCase() + key.slice(1)}: +{value}
+                      </li>
+                    ))}
                   </ul>
                 </div>
-              )}
-            </div>
+              )
           )}
+        </div>
+
+        {selectedClass.name && (
+          <div className="mt-4 bg-[var(--color6)] rounded-xl p-4 text-lg">
+            <p className="text-4xl mb-4 text-[var(--color4)]">
+              Bonificadores de Clase
+            </p>
+            <ul className="text-2xl">
+              <div className="flex justify-center gap-4 mb-4">
+                <p>• Dado de golpe: </p>
+                <li>{selectedClass.hitDie}</li>
+              </div>
+              <div className="flex  justify-center gap-4 mb-4">
+                <p>• Habilidad primaria: </p>
+                <li>{selectedClass.primaryAbility}</li>
+              </div>
+              <div className="flex justify-center gap-4 mb-4">
+                <p>• Armas: </p>
+                <li>{selectedClass.weapons}</li>
+              </div>
+              <div className="flex  justify-center gap-4 mb-4">
+                <p>• Tiradas de salvación: </p>
+                <li>{selectedClass.savingThrows?.join(", ")}</li>
+              </div>
+              <div className="flex justify-center gap-4 mb-4">
+                <p>• Rasgos: </p>
+                <li>{selectedClass.features?.join(", ")}</li>
+              </div>
+            </ul>
+            {selectedSubclass.name && (
+              <div className="mt-4 bg-[var(--color6)] rounded-xl p-4 text-lg">
+                <p className="text-4xl mb-4 text-[var(--color4)]">
+                  Bonificadores de Subclase
+                </p>
+                <ul className="text-2xl">
+                  <li>• Rasgos: {selectedSubclass.features?.join(", ")}</li>
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+        <div>
+          <h2 className="mt-8 text-4xl">Escribe tu historia</h2>
+          <textarea
+            className="mt-4 w-full rounded-xl bg-[var(--color6)] p-4 text-left placeholder:text-center"
+            placeholder="Empieza a dar aquí tus primeros pasos..."
+          ></textarea>
         </div>
       </div>
     </div>
