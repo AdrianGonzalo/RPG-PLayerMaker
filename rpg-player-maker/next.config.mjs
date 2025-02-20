0a951bdca2468780b94f25cc22e15b98c6446ce4
Adrián <adriangonzalodev@gmail.com>
2025-02-20 18:39:56 +0100

    Fix: Ajuste en basePath y assetPrefix para deploy

--- rpg-player-maker/next.config.mjs
+++ rpg-player-maker/next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/rpg-player-maker" : "",
+  assetPrefix: isProd ? "/rpg-player-maker/" : "",
+};
 
 export default nextConfig;
