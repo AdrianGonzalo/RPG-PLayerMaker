843f72a5ec0fd334b16328a3517513b5dcee6c2e
Adrián <adriangonzalodev@gmail.com>
2025-02-21 02:07:09 +0100

    Get init the backend #5

--- rpg-player-maker/next.config.mjs
+++ rpg-player-maker/next.config.mjs
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  ...(isProd && !process.env.USE_API ? { output: "export" } : {}),
+
   basePath: isProd ? "/RPG-PLayerMaker" : "",
   assetPrefix: isProd ? "/RPG-PLayerMaker/" : "",
   images: {
---
+++ rpg-player-maker/src/pages/api/hello.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json({ message: "Por la mañana cafe por la tarde ron" });
+}
